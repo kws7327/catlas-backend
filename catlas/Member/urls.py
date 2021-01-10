abd1ae334e5c3b13516c88945d17d8e19ad28e26
kws7327 <kws7327@naver.com>
2021-01-10 19:40:14 +0900

    21.01.10
    Class-based Views

--- catlas/Member/urls.py
+++ catlas/Member/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.urls import path
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
@@ -8,7 +9,8 @@ urlpatterns = [
     url(r'^index', views.index, name='index'),
 #   url(r'^register', views.register, name='register'),
     url(r'^login/$', views.signin, name='login'),
-    path('members/', views.Member_list),
-    path('members/<int:pk>/', views.Member_detail),
+    path('members/', views.MemberList.as_view()),
+    path('members/<int:pk>/', views.MemberDetail.as_view()),
 
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
--- catlas/Member/views.py
+++ catlas/Member/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework import status
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
+#from django.http import Http404
+#from rest_framework import status
+#from rest_framework.views import APIView
+#from rest_framework.response import Response
 from .serializers import MemberSerializer
 from Member.models import Member
-
+from rest_framework import generics
 
 def index(request):
     return render(request, 'index.html', {})
@@ -15,45 +14,60 @@ def index(request):
 def signin(request):
     return render(request, 'login.html', {})
 
-@api_view(['GET','POST'])
 
-def Member_list(request):
 
-    if request.method == 'GET':
+class MemberList(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+
+"""
+
+class Memberlist(APIView):
+
+    def get(self, request, format=None):
         members = Member.objects.all()
         serializer = MemberSerializer(members, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self,request,format=None):
         serializer = MemberSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def Member_detail(request, pk):
+class Memberdetail(APIView):
 
-    try:
-        member = Member.objects.get(pk=pk)
-    except Member.DoesNotExist:
-        return HttpResponse(status=404)
+    def get_object(self, pk):
+        try:
+            return Member.objects.get(pk=pk)
+        except Member.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        member = self.net_object(pk)
         serializer = MemberSerializer(member)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = MemberSerializer(member, data=data)
+    def put(self, request, pk, format=None) :
+        member = self.get_object(pk)
+        serializer = MemberSerializer(member, data=request.data)
         if serializer.isvaild():
             serializer.save()
-            return JsonResponse()
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        member = self.net_object(pk)
         member.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
 
     #serializer = MemberSerializer()
@@ -61,4 +75,4 @@ def Member_detail(request, pk):
     #return HttpResponse(repr(serializer))
 
 
-
+"""
